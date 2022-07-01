7d9280a69f04166fc5f9521fb1e96ec08503c28f
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-07-01 17:57:14 +0530

    .

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef,useState } from "react";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import EmailIcon from "@mui/icons-material/Email";
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
-import ReactPlayer from "react-player"
-import mg from "./videos/1mg.mp4";
-import bookmyshow from "./videos/bookmyshow.mp4";
-import dineout from "./videos/dineout.mp4"
+
 
 const Projects = () => {
 
@@ -13,36 +10,7 @@ const Projects = () => {
 
       <div>
         <div className="project">
-          <ReactPlayer
-           url={mg}
-           loop={true}
-           playing={true}
-           muted={true}
-           playbackRate={0.5}
-           controls={false}
-           config={{ file: { 
-            attributes: {
-              controlsList: 'nodownload'  
-            }
-        }}}
-           onContextMenu={e => e.preventDefault()}
-           onEnded={()=>this.onEnded()}
-          />
-          <ReactPlayer
-           url={bookmyshow}
-           muted={true}
-           playbackRate={0.5}
-           loop={true}
-           playing={true}
-           controls={false}
-           config={{ file: { 
-            attributes: {
-              controlsList: 'nodownload' 
-            }
-        }}}
-        onContextMenu={e => e.preventDefault()}
-           onEnded={()=>this.onEnded()}
-          />
+         
         </div>
 
       </div>
